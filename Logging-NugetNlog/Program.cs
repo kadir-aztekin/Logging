2d853c99865f7a8fcd0c5a96e122559d6c12529f
kadir-aztekin <aztekadir@icloud.com>
2022-09-27 16:42:46 +0300

    Add project files.

---
+++ Logging-NugetNlog/Program.cs
@@ -0,0 +1,33 @@
+﻿using NLog;
+using System;
+
+namespace Logging_NugetNlog
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Commander test = new Commander();
+            test.StartTransaction();
+            test.StopTransaction();
+            test.DoSomething();
+        }
+    }
+     class Commander
+    {
+        Logger logger = LogManager.GetCurrentClassLogger();
+        public void StartTransaction()
+        {
+            logger.Info("Transaction başlatıldı");
+        }
+        public void DoSomething()
+        {
+            logger.Fatal("Abc servisi çalışmıyor");
+            logger.Error("Db bağlantısı sağlanamadı");
+        }
+        public void StopTransaction()
+        {
+            logger.Warn("Transaction sonlandı fakat işlemler çalışmıyor");
+        }
+    }
+}
---
+++ Logging/Program.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.IO;
+
+namespace Logging
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Loglama();
+        }
+
+        private static void Loglama()
+        {
+            try
+            {
+                Console.WriteLine("Bir sayı giriniz");
+                int sayı = Convert.ToInt32(Console.ReadLine());
+                if (sayı == 3)
+                {
+                    throw new Exception("Yanlış Sayı");
+                }
+            }
+            catch (Exception hata)
+            {
+                File.AppendAllText("log.txt", Environment.UserName);
+                File.AppendAllText("log.txt", Environment.NewLine);
+                File.AppendAllText("log.txt", DateTime.Now.ToString("dd,MM,yyyy HH:mm"));
+                File.AppendAllText("log.txt","\r\n");
+                File.AppendAllText("log.txt", hata.Message);
+                File.AppendAllText("log.txt", "@");
+                File.AppendAllText("log.txt", hata.StackTrace);
+                File.AppendAllText("log.txt", Environment.NewLine+"*****"+Environment.NewLine);
+
+
+
+
+
+
+
+            }
+
+        }
+    }
+}
